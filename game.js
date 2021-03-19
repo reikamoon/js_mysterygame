b005aa204f652d6bfa336e74b8a98a948da51fc0
Anjelica <ambrosio.anjelica@gmail.com>
2021-03-19 15:18:43 -0700

    finished meet the cast

--- game.js
+++ game.js
@@ -994,6 +994,24 @@ const textNodes = [
   },
 
   {
+    id: 309,
+    name: "Valeria",
+    text: "Would you like to read another entry?",
+    portrait: "/assets/busts/Valeria/Valeria4.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 300
+      },
+
+      {
+        text: "Next (Dad)",
+        nextText: 310
+      },
+    ]
+  },
+
+  {
     id: 310,
     name: "Dad",
     text: "I am your father. I used to be a pretty big deal back in the day as an artist, but I'm retired now. I just mentor young artists nowadays.",
@@ -1010,6 +1028,871 @@ const textNodes = [
       },
     ]
   },
+
+  {
+    id: 310,
+    name: "Dad",
+    text: "Sam, I am your father. I used to be a pretty big deal back in the day as an artist, but I'm retired now. I just mentor young artists nowadays.",
+    portrait: "/assets/busts/Dad/Dad.png",
+    options: [
+      {
+        text: "Special Skills",
+        nextText: 311
+      },
+
+      {
+        text: "Relationships",
+        nextText: 312
+      },
+    ]
+  },
+
+  {
+    id: 311,
+    name: "Dad",
+    text: "Skills? I'm your dad, I can do anything. ...Just kidding. I can make a good barbecue, and um...I know how to discern fake paintings.",
+    portrait: "/assets/busts/Dad/Dad.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 310
+      },
+
+      {
+        text: "Next",
+        nextText: 313
+      },
+    ]
+  },
+
+  {
+    id: 312,
+    name: "Dad",
+    text: "I'm your father, and Elijah and Arius's mentor. I see them as sons to me. Let's see...I knew Valeria back when she was attempting to make it in the art world. Lena is a journalist that's been hounding me for a bit, and Connor's a bit of a 'stan' of me? Is that how you young people talk?",
+    portrait: "/assets/busts/Dad/Dad.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 310
+      },
+
+      {
+        text: "Next",
+        nextText: 313
+      },
+    ]
+  },
+
+  {
+    id: 313,
+    name: "Dad",
+    text: "Care to read another entry?",
+    portrait: "/assets/busts/Dad/Dad.png",
+    options: [
+      {
+        text: "Back to Entries",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Sam)",
+        nextText: 314
+      },
+    ]
+  },
+
+  {
+    id: 314,
+    name: "Sam [Player]",
+    text: "My name is Sam...I don't remember much what happened at the party, but I do know that I was the last person seen with the victim, Arius. I need to prove my innocence before the killer strikes again.",
+    portrait: "",
+    options: [
+      {
+        text: "Special Skills",
+        nextText: 315
+      },
+
+      {
+        text: "Relationships",
+        nextText: 316
+      },
+    ]
+  },
+
+  {
+    id: 315,
+    name: "Sam [Player]",
+    text: "Skills...I guess I have a close eye to detail. Maybe a little confrontational, but I can try to get along with people, right?",
+    portrait: "",
+    options: [
+      {
+        text: "Back",
+        nextText: 314
+      },
+
+      {
+        text: "Next",
+        nextText: 317
+      },
+    ]
+  },
+
+  {
+    id: 316,
+    name: "Sam [Player]",
+    text: "Relationships, huh? I have my dad...and I don't remember much else, sorry.",
+    portrait: "",
+    options: [
+      {
+        text: "Back",
+        nextText: 314
+      },
+
+      {
+        text: "Next",
+        nextText: 317
+      },
+    ]
+  },
+
+  {
+    id: 317,
+    name: "Sam [Player]",
+    text: "Read another entry?",
+    portrait: "",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Arius)",
+        nextText: 318
+      },
+    ]
+  },
+
+  {
+    id: 318,
+    name: "Arius",
+    text: "Name's Arius. My name means 'immortal', so I'm not dying anytime soon, right?",
+    portrait: "/assets/busts/Arius/Arius.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 319
+      },
+
+      {
+        text: "Relationships",
+        nextText: 320
+      },
+    ]
+  },
+
+  {
+    id: 319,
+    name: "Arius",
+    text: "...Art. It's about time people realized how great my art is. No, art styles change consistently, I didn't cheat or anything...",
+    portrait: "/assets/busts/Arius/Arius.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 318
+      },
+
+      {
+        text: "Next",
+        nextText: 321
+      },
+    ]
+  },
+
+  {
+    id: 320,
+    name: "Arius",
+    text: "Let's see...Auntie Valeria. She married my uncle, who is my dad's brother. Dante and Sabrina are my best friends...when Dante isn't being a complete jerk. Katalina, his sister...it's complicated.",
+    portrait: "/assets/busts/Arius/Arius.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 318
+      },
+
+      {
+        text: "Next",
+        nextText: 321
+      },
+    ]
+  },
+
+  {
+    id: 321,
+    name: "Arius",
+    text: "Read another entry?",
+    portrait: "/assets/busts/Arius/Arius.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Dante)",
+        nextText: 322
+      },
+    ]
+  },
+
+  {
+    id: 322,
+    name: "Dante",
+    text: "...What? I'm Dante.",
+    portrait: "/assets/busts/Dante/Dante.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 323
+      },
+
+      {
+        text: "Relationships",
+        nextText: 324
+      },
+    ]
+  },
+
+  {
+    id: 323,
+    name: "Dante",
+    text: "My special skills include knowing when to shut up and mind my own business. I'd be happy to mentor you.",
+    portrait: "/assets/busts/Dante/DanteConfident.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 322
+      },
+
+      {
+        text: "Next",
+        nextText: 325
+      },
+    ]
+  },
+
+  {
+    id: 324,
+    name: "Dante",
+    text: "Relationships? Tch. I have a younger sister, Katalina...I have two flies named Arius and Sabrina that pester me and tell me to play nice. And...Valeria's pretty, isn't she?",
+    portrait: "/assets/busts/Dante/DanteHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 322
+      },
+
+      {
+        text: "Next",
+        nextText: 325
+      },
+    ]
+  },
+
+  {
+    id: 325,
+    name: "Dante",
+    text: "You wanna read another entry or what?",
+    portrait: "/assets/busts/Dante/Dante.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Katalina)",
+        nextText: 326
+      },
+    ]
+  },
+
+  {
+    id: 326,
+    name: "Katalina",
+    text: "Um...hi...I'm...Katalina. I'm...studying to be a nurse...",
+    portrait: "/assets/busts/Katalina/Katalina.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 327
+      },
+
+      {
+        text: "Relationships",
+        nextText: 328
+      },
+    ]
+  },
+
+  {
+    id: 327,
+    name: "Katalina",
+    text: "I...don't think I'm great. I...I can calm my brother down...I think...",
+    portrait: "/assets/busts/Katalina/KatalinaSad.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 326
+      },
+
+      {
+        text: "Next",
+        nextText: 329
+      },
+    ]
+  },
+
+  {
+    id: 328,
+    name: "Katalina",
+    text: "My brother...Dante, is very mean...but lucky, to have friends like Arius and Sabrina. Arius...used to be my boyfriend...but he and Dante fought...",
+    portrait: "/assets/busts/Katalina/KatalinaHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 326
+      },
+
+      {
+        text: "Next",
+        nextText: 329
+      },
+    ]
+  },
+
+  {
+    id: 329,
+    name: "Katalina",
+    text: "Um...did you want to read more?",
+    portrait: "/assets/busts/Katalina/Katalina.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Vivian)",
+        nextText: 330
+      },
+    ]
+  },
+
+  {
+    id: 330,
+    name: "Vivian",
+    text: "Hiiiii~! I'm Vivian, but call me Vivi or Viva! I'm Oskar's girlfriend, and I won't let ANYONE slander his name!",
+    portrait: "/assets/busts/Vivian/VivianHappy2.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 331
+      },
+
+      {
+        text: "Relationships",
+        nextText: 332
+      },
+    ]
+  },
+
+  {
+    id: 331,
+    name: "Vivian",
+    text: "I'm super duper good at being a cheerleader. I cheer on Oskar all the time while he paints! Ain't he the greatest?!",
+    portrait: "/assets/busts/Vivian/Vivian.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 330
+      },
+
+      {
+        text: "Next",
+        nextText: 333
+      },
+    ]
+  },
+
+  {
+    id: 332,
+    name: "Vivian",
+    text: "I'm Oskar's one and only, y'hear?! Don't get any ideas. I don't care too much for that sourpuss, Dante, I guess. Also, Arius really pisses me off! How dare he steal Oskar's victory?!",
+    portrait: "/assets/busts/Vivian/VivianAngry.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 330
+      },
+
+      {
+        text: "Next",
+        nextText: 333
+      },
+    ]
+  },
+
+  {
+    id: 333,
+    name: "Vivian",
+    text: "Wanna read another entry?",
+    portrait: "/assets/busts/Vivian/Vivian.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Oskar)",
+        nextText: 334
+      },
+    ]
+  },
+
+  {
+    id: 334,
+    name: "Oskar",
+    text: "Salutations. I am Oskar. You may have heard of my work as an artist. Please, do not refer to me as a prodigy or genius...it is embarrassing.",
+    portrait: "/assets/busts/Oskar/Oskar.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 335
+      },
+
+      {
+        text: "Relationships",
+        nextText: 336
+      },
+    ]
+  },
+
+  {
+    id: 335,
+    name: "Oskar",
+    text: "You are interested in my skillset? I am always happy to help. I did some side work as a police sketch artist before. You can describe anything and I will draw it in less than ten minutes.",
+    portrait: "/assets/busts/Oskar/OskarHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 334
+      },
+
+      {
+        text: "Next",
+        nextText: 337
+      },
+    ]
+  },
+
+  {
+    id: 336,
+    name: "Oskar",
+    text: "I try to get along with everyone, generally. Vivi is my significant other, but I must admit, she is a tad too overprotective over my wellbeing. I...generally try to avoid Lena. She is rather nosy, even for a journalist. I also admire Elijah's work greatly...and Arius's, I suppose.",
+    portrait: "/assets/busts/Oskar/OskarHappy2.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 334
+      },
+
+      {
+        text: "Next",
+        nextText: 337
+      },
+    ]
+  },
+
+  {
+    id: 337,
+    name: "Oskar",
+    text: "Care to read another entry?",
+    portrait: "/assets/busts/Oskar/Oskar.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Elijah)",
+        nextText: 338
+      },
+    ]
+  },
+
+  {
+    id: 338,
+    name: "Elijah",
+    text: "Hey, what's up? I'm Elijah. Future big shot in the art industry.",
+    portrait: "/assets/busts/Elijah/Elijah.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 339
+      },
+
+      {
+        text: "Relationships",
+        nextText: 340
+      },
+    ]
+  },
+
+  {
+    id: 339,
+    name: "Elijah",
+    text: "Skills, huh? I'm really good at a lot of things, hehe. Maybe I should give you a demo? Just kidding. i guess sculpting is my favorite hobby. I'm trying to make it big as a sculptor.",
+    portrait: "/assets/busts/Elijah/ElijahWink2.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 338
+      },
+
+      {
+        text: "Next",
+        nextText: 341
+      },
+    ]
+  },
+
+  {
+    id: 340,
+    name: "Elijah",
+    text: "Why, you interested in me? I am single, y'know. But I'm your dad's mentee, so it'd be kinda weird. Plus, I guess Oskar and Arius are rivals of mine in the art industry, but I guess Arius's annoying mug is gone, so guess I gotta take down that egghead, Oskar. ...I'm kidding.",
+    portrait: "/assets/busts/Elijah/ElijahHappy2.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 338
+      },
+
+      {
+        text: "Next",
+        nextText: 341
+      },
+    ]
+  },
+
+  {
+    id: 341,
+    name: "Elijah",
+    text: "Leaving so soon? You gonna read about someone else to compare them to me? Careful...I'm one of a kind.",
+    portrait: "/assets/busts/Elijah/ElijahHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Sabrina)",
+        nextText: 342
+      },
+    ]
+  },
+
+  {
+    id: 342,
+    name: "Sabrina",
+    text: "Hiya. I'm Sabrina. I'm currently studying to be a nurse. ...Yeah, like Kat.",
+    portrait: "/assets/busts/Sabrina/Sabrina.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 343
+      },
+
+      {
+        text: "Relationships",
+        nextText: 344
+      },
+    ]
+  },
+
+  {
+    id: 343,
+    name: "Sabrina",
+    text: "Skills? Yeah, I got a lot of 'em. I can play guitar, knit, and bake a mean pie. I'm also pretty good with mediating fights. ...I got a lot of experience.",
+    portrait: "/assets/busts/Sabrina/SabrinaHappy2.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 342
+      },
+
+      {
+        text: "Next",
+        nextText: 345
+      },
+    ]
+  },
+
+  {
+    id: 344,
+    name: "Sabrina",
+    text: "Yeah. I'm supposed to be the bestie to Arius and Dante, but sometimes I feel like I'm their babysitter.Dante is so high strung and Arius often has his head in the clouds. Also, Elijah irritates me to no end. I can't stand flirts like him. I'm glad Ari put him in his place.",
+    portrait: "/assets/busts/Sabrina/SabrinaAngry2.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 342
+      },
+
+      {
+        text: "Next",
+        nextText: 345
+      },
+    ]
+  },
+
+  {
+    id: 345,
+    name: "Sabrina",
+    text: "All done? Do you wanna read more?",
+    portrait: "/assets/busts/Sabrina/Sabrina.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Lena)",
+        nextText: 346
+      },
+    ]
+  },
+
+  {
+    id: 346,
+    name: "Lena",
+    text: "Oh, yeah! Jackpot! Hey, I'm Lena, a journalist. Can I ask you some questions about your dad?",
+    portrait: "/assets/busts/Lena/LenaHappy2.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 347
+      },
+
+      {
+        text: "Relationships",
+        nextText: 348
+      },
+    ]
+  },
+
+  {
+    id: 347,
+    name: "Lena",
+    text: "I'm something of an investigator myself. I can sniff out clues better than a bloodhound!",
+    portrait: "/assets/busts/Lena/Lena.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 346
+      },
+
+      {
+        text: "Next",
+        nextText: 349
+      },
+    ]
+  },
+
+  {
+    id: 348,
+    name: "Lena",
+    text: "Sure. Elijah's a buddy of mine. Don't let his honeyed words get to you, he's playing most of the time, I think. But yeah, Elijah's connection with your dad has made it really easy to get in contact with these art types. ",
+    portrait: "/assets/busts/Lena/LenaHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 346
+      },
+
+      {
+        text: "Next",
+        nextText: 349
+      },
+    ]
+  },
+
+  {
+    id: 349,
+    name: "Lena",
+    text: "Digging up some dirt, huh? Lemme know if you find anything juicy!",
+    portrait: "/assets/busts/Lena/LenaHappy.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Connor)",
+        nextText: 350
+      },
+    ]
+  },
+
+  {
+    id: 350,
+    name: "Connor",
+    text: "Hey. I'm Connor, I'm 26, and a computer programmer for a tech company. Need something?",
+    portrait: "/assets/busts/Connor/Connor.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 351
+      },
+
+      {
+        text: "Relationships",
+        nextText: 352
+      },
+    ]
+  },
+
+  {
+    id: 351,
+    name: "Connor",
+    text: "Python, JavaScript, Java, C++, oh, you meant THOSE kind. Er, I'm really good with computers and tech. Hacking isn't too hard, so if you show me a security camera I might be able to get the footage.",
+    portrait: "/assets/busts/Connor/Connor.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 350
+      },
+
+      {
+        text: "Next",
+        nextText: 353
+      },
+    ]
+  },
+
+  {
+    id: 352,
+    name: "Connor",
+    text: "I'm a fan of your father's work, and I'm well-acquainted with Valeria. She has commissioned me a lot back when I was a contract web developer.",
+    portrait: "/assets/busts/Connor/Connor.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 350
+      },
+
+      {
+        text: "Next",
+        nextText: 353
+      },
+    ]
+  },
+
+  {
+    id: 353,
+    name: "Connor",
+    text: "I know, I'm not that interesting...want to read something else?",
+    portrait: "/assets/busts/Connor/ConnorSad.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Aurelia)",
+        nextText: 354
+      },
+    ]
+  },
+
+  {
+    id: 354,
+    name: "Aurelia",
+    text: "Hello, darling! I'm a pretty big deal in the fashion industry. I have my own high end brand...that I doubt someone like you could afford.",
+    portrait: "/assets/busts/Aurelia/Aurelia.png",
+    options: [
+      {
+        text: "Skills",
+        nextText: 355
+      },
+
+      {
+        text: "Relationships",
+        nextText: 356
+      },
+    ]
+  },
+
+  {
+    id: 355,
+    name: "Aurelia",
+    text: "I'm really great with crafting cute little disguises. But I don't work for free, you know.",
+    portrait: "/assets/busts/Aurelia/AureliaSurprised.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 354
+      },
+
+      {
+        text: "Next",
+        nextText: 357
+      },
+    ]
+  },
+
+  {
+    id: 356,
+    name: "Aurelia",
+    text: "Ooh, curious are we? No, honey. I'm single. You're a little young for my taste. My sister is that dim wit Valeria...oh and if you have any eyes on Elijah...I advise you not.",
+    portrait: "/assets/busts/Aurelia/AureliaSurprised.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 354
+      },
+
+      {
+        text: "Next",
+        nextText: 357
+      },
+    ]
+  },
+
+  {
+    id: 357,
+    name: "Aurelia",
+    text: "What, am I not interesting enough for you? You want to read about someone else?",
+    portrait: "/assets/busts/Aurelia/AureliaAngry.png",
+    options: [
+      {
+        text: "Back",
+        nextText: 301
+      },
+
+      {
+        text: "Next (Valeria)",
+        nextText: 306
+      },
+    ]
+  },
+
 
 
 
